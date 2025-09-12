2138530c8bb3b3dd1ba57640f8815549487da9e7
egerius <e.yanin@list.ru>
2025-09-12 13:54:37 +0300

    Initial commit

---
+++ lab1/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+
+@app.get("/", response_class=HTMLResponse)
+def hello_world():
+    return """
+    <html>
+        <head><title>Hello World</title></head>
+        <body>
+            <h1>Hello, World!</h1>
+        </body>
+    </html>
+    """
